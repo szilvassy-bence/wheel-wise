8ff50f8e38fd411c66ad5818a46ce02361781cd5
Laszlo Szijarto <szillas92@gmail.com>
2024-04-25 09:53:31 +0200

    Added Status: New to the useState in Created

--- wheel-wise-frontend/src/Pages/CreateAdvertisement/CreateAd.jsx
+++ wheel-wise-frontend/src/Pages/CreateAdvertisement/CreateAd.jsx
@@ -12,7 +12,7 @@ export default function CreateAd(){
     const[user, setUser] = useState({});
 
     const [formData, setFormData] = useState({
-        brand: "", model: "", color:"", fuelType:"", transmission:"", status:"", year: 0, price: 0, mileage: 0, power: 0
+        brand: "", model: "", color:"", fuelType:"", transmission:"", status:"New", year: 0, price: 0, mileage: 0, power: 0
     })
 
     const [checkedItems, setCheckedItems] = useState({});
